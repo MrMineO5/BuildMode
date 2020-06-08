5087a772102d8f109a875552ee4d221415ecf41b
BuildTools <unconfigured@null.spigotmc.org>
2020-06-08 17:44:16 +0200

    Initial commit

---
+++ src/main/java/xyz/ultradev/buildmode/BuildMode.java
@@ -0,0 +1,15 @@
+package xyz.ultradev.buildmode;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BuildMode extends JavaPlugin {
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+
+
+}
---
+++ src/main/java/xyz/ultradev/buildmode/BuildModeModule.java
@@ -0,0 +1,21 @@
+package xyz.ultradev.buildmode;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+@Getter
+@AllArgsConstructor
+public abstract class BuildModeModule {
+    private BuildMode parent;
+    private String name;
+
+    public boolean shouldEnable() {
+        return getParent().getConfig()
+                .getBoolean("integrations." + getName(), false);
+    }
+    public abstract boolean canEnable();
+    public abstract boolean canBuild(Player pl, Location loc);
+
+}
